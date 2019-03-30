b1d16785c6b680b8af943addff55f20b5fcf6c56
Luis Ferreira <hidnasio@gmail.com>
2019-03-30 16:02:10 -0300

    Add circle ci config

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Talar.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.5", only: [:dev, :test], runtime: false},
       {:ecto_sql, "~> 3.0"},
       {:ex_guard, "~> 1.3", only: :dev},
       {:gettext, "~> 0.11"},
